f8649ae36e8bfcda988fff9a525a14f17331d802
Amine Raouf <raouf.amine@iisgalvanimi.edu.it>
2024-03-18 12:58:37 +0000

    prime schede

--- Esercizio6/src/app/lista-giocatori/lista-giocatori.component.ts
+++ Esercizio6/src/app/lista-giocatori/lista-giocatori.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'lista-giocatori',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './lista-giocatori.component.html',
   styleUrl: './lista-giocatori.component.css'
 })
