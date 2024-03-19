b587096c02be090f382e3e8a13ea06e0929063f0
Amine Raouf <raouf.amine@iisgalvanimi.edu.it>
2024-03-19 08:37:19 +0000

    punto 6 da finire

--- Esercizio6/src/app/lista-giocatori/lista-giocatori.component.ts
+++ Esercizio6/src/app/lista-giocatori/lista-giocatori.component.ts
@@ -11,10 +11,17 @@ import { Component } from '@angular/core';
 export class ListaGiocatoriComponent {
   giocatori = [
     
-    {nome: 'ProgamingStudioHD'},
-    {nome: 'GTASERIES Videos'},
-    {nome: 'MRBOSSFTW'},
-    {nome: 'echno Gamerz'}
+    {nome: 'PewDiePie', eta: 19, nazionalita: 'SVEDESE', personaggio: 'FRANKLIN', foto: 'https://i.pinimg.com/736x/07/9f/88/079f8825deb2e6b1d51805ede2fa51be.jpg'},
+    {nome: 'VanossGaming', eta: 19, nazionalita: 'CANADESE', personaggio: 'FRANKLIN', foto: 'https://i.pinimg.com/736x/07/9f/88/079f8825deb2e6b1d51805ede2fa51be.jpg'},
+    {nome: 'Kwebbelkop', eta: 28, nazionalita: 'OLANDESE', personaggio: 'FRANKLIN', foto: 'https://i.pinimg.com/736x/07/9f/88/079f8825deb2e6b1d51805ede2fa51be.jpg'},
+    {nome: 'Typical Gamer', eta: 31, nazionalita: 'CANADESE', personaggio: 'FRANKLIN', foto: 'https://i.pinimg.com/736x/07/9f/88/079f8825deb2e6b1d51805ede2fa51be.jpg'},
+    {nome: 'Jelly', eta: 27, nazionalita: 'OLANDESE', personaggio: 'FRANKLIN', foto: 'https://i.pinimg.com/736x/07/9f/88/079f8825deb2e6b1d51805ede2fa51be.jpg'},
+    {nome: 'MrBossFTW', eta: 30, nazionalita: 'AMERICANO', personaggio: 'FRANKLIN', foto: 'https://i.pinimg.com/736x/07/9f/88/079f8825deb2e6b1d51805ede2fa51be.jpg'},
+    {nome: 'Slogoman', eta: 26, nazionalita: 'INGLESE', personaggio: 'FRANKLIN', foto: 'https://i.pinimg.com/736x/07/9f/88/079f8825deb2e6b1d51805ede2fa51be.jpg'},
+    {nome: 'GTA Series Videos', eta: 27, nazionalita: 'AMERICANO', personaggio: 'FRANKLIN', foto: 'https://i.pinimg.com/736x/07/9f/88/079f8825deb2e6b1d51805ede2fa51be.jpg'},
+    {nome: 'iCrazyTeddy', eta: 23, nazionalita: 'AMERICANO', personaggio: 'FRANKLIN', foto: 'https://i.pinimg.com/736x/07/9f/88/079f8825deb2e6b1d51805ede2fa51be.jpg'},
+    {nome: 'GTA V Machinima', eta: 28, nazionalita: 'CANADESE', personaggio: 'FRANKLIN', foto: 'https://i.pinimg.com/736x/07/9f/88/079f8825deb2e6b1d51805ede2fa51be.jpg'}
+    
     
   ]
 }
