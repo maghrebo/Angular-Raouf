eb75344898a504c2b33fbe1b6785e48166958f60
Amine Raouf <raouf.amine@iisgalvanimi.edu.it>
2024-03-21 13:53:30 +0000

    punto 7

---
+++ Esercizio7/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FirstComponentComponent } from './first-component/first-component.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FirstComponentComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Esercizio7';
+}
