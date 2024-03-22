20753d76aa6b4344a106d2d90d8f0cd041f1d130
Amine Raouf <raouf.amine@iisgalvanimi.edu.it>
2024-03-22 12:12:40 +0000

    design e primi dati bottone

--- Esercizio7/src/app/first-component/first-component.component.ts
+++ Esercizio7/src/app/first-component/first-component.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-first-component',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './first-component.component.html',
   styleUrl: './first-component.component.css'
 })
 export class FirstComponentComponent {
 
+  info: boolean = false;
+
+  button_testo_1 = "Snivy (di tipo Erba)";
+  button_testo_2 = "Tepig (di tipo Fuoco)";
+  button_testo_3 = "Oshawott (di tipo Acqua)"
+
+  mostra_info() {
+    this.info = !this.info;
+  }
 }
