ce5e79738b17fde8e809456129c489d3868d2b5d
Amine Raouf <raouf.amine@iisgalvanimi.edu.it>
2024-04-18 12:52:02 +0000

    navbar

--- Esercizio10/src/app/app.component.ts
+++ Esercizio10/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IntroductionComponent, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
