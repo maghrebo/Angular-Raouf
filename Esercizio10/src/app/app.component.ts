a16ffd8ea8b06df28467c5ff751041c5f0d8a15c
Amine Raouf <raouf.amine@iisgalvanimi.edu.it>
2024-04-23 10:55:54 +0000

    correzione eliminazione navigation component

--- Esercizio10/src/app/app.component.ts
+++ Esercizio10/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IntroductionComponent } from './introduction/introduction.component';
-import { NavigationComponent } from './navigation/navigation.component';
 import { MostreComponent } from './mostre/mostre.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, IntroductionComponent, NavigationComponent, MostreComponent, CommonModule],
+  imports: [RouterOutlet, IntroductionComponent, MostreComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -18,9 +17,13 @@ export class AppComponent {
   mostra_home = true
   mostra_mostre = false
 
-  onRiceviDati(value: any){
+  home() {
+    this.mostra_home = true
+    this.mostra_mostre = false
+  }
 
-    this.mostra_home = value
- 
+  mostre() {
+    this.mostra_mostre = true
+    this.mostra_home = false
   }
 }
